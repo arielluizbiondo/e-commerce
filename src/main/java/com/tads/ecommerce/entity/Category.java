85ec2f669a185bd3964d4eea98169fe6424de63b
Ariel Biondo <a.biondo@estudante.ifmt.edu.br>
2026-08-17 21:55:40 -0400

    Atualiza entidade Category - tabela

--- src/main/java/com/tads/ecommerce/entity/Category.java
+++ src/main/java/com/tads/ecommerce/entity/Category.java
@@ -1,8 +1,20 @@
 package com.tads.ecommerce.entity;
 
-public class Category {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "categoria")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Category
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
-
-    
 }
